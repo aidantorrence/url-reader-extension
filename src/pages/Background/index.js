79a97df53bfa49816f1ee5de15629bc68970deac
Aidan Torrence <aidan.torrence@gmail.com>
2022-01-20 18:31:32 -0500

    get url

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,2 +1,21 @@
-console.log('This is the background page.');
-console.log('Put the background scripts here.');
+
+
+console.log('howdy doody')
+chrome.tabs.onUpdated.addListener( async function (tabId, changeInfo, tab) {
+  if (changeInfo.status === 'complete') {
+      const tabs = await chrome.tabs.query({active: true, currentWindow: true})
+      console.log(tabs);
+      fetch('https://localhost:8080/checkBackground')
+    // const timeout = setTimeout( async () => {
+    // console.log('helo')
+    // //     console.log('hello?')
+    // //   console.log(await chrome.tabs.query({active: true, currentWindow: true}))
+    // }, 1000 )
+
+    // return clearTimeout(timeout);
+
+//   let queryOptions = { active: true, currentWindow: true };
+//   let [tab] = await chrome.tabs.query(queryOptions);
+//   console.log(tab)
+  }
+})
