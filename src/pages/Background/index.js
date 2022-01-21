6696b3603ff81159e795465588abec0e9f8fdbcf
Aidan Torrence <aidan.torrence@gmail.com>
2022-01-20 19:31:41 -0500

    socketio

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,11 +1,14 @@
+import io from "socket.io-client";
 
+const socket = io("http://localhost:4000");
 
 console.log('howdy doody')
 chrome.tabs.onUpdated.addListener( async function (tabId, changeInfo, tab) {
   if (changeInfo.status === 'complete') {
       const tabs = await chrome.tabs.query({active: true, currentWindow: true})
-      console.log(tabs);
-      fetch('https://localhost:8080/checkBackground')
+      console.log('yoooo', tabs);
+      socket.emit(`url: ${tabs[0].url}` );
+      // fetch('http://localhost:8080/')
     // const timeout = setTimeout( async () => {
     // console.log('helo')
     // //     console.log('hello?')
