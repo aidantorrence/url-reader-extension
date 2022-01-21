37961ed217828bd0e064d3137f2113caba1bd702
Aidan Torrence <aidan.torrence@gmail.com>
2022-01-20 23:39:21 -0500

    add getting of url

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,24 +1,14 @@
-import io from "socket.io-client";
+chrome.tabs.onActivated.addListener( function(activeInfo){
+    chrome.tabs.get(activeInfo.tabId, function(tab){
+        y = tab.url;
+        console.log("you are here: "+y);
+        fetch('http://localhost:8080/storeUrl', { body: JSON.stringify({url: y}), method: 'POST', headers: { 'Content-Type': 'application/json' } })
+    });
+});
 
-const socket = io("http://localhost:4000");
-
-console.log('howdy doody')
-chrome.tabs.onUpdated.addListener( async function (tabId, changeInfo, tab) {
-  if (changeInfo.status === 'complete') {
-      const tabs = await chrome.tabs.query({active: true, currentWindow: true})
-      console.log('yoooo', tabs);
-      socket.emit(`url: ${tabs[0].url}` );
-      // fetch('http://localhost:8080/')
-    // const timeout = setTimeout( async () => {
-    // console.log('helo')
-    // //     console.log('hello?')
-    // //   console.log(await chrome.tabs.query({active: true, currentWindow: true}))
-    // }, 1000 )
-
-    // return clearTimeout(timeout);
-
-//   let queryOptions = { active: true, currentWindow: true };
-//   let [tab] = await chrome.tabs.query(queryOptions);
-//   console.log(tab)
-  }
-})
+chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
+    if (tab.active && change.url) {
+        console.log("you are here: "+change.url); 
+        fetch('http://localhost:8080/storeUrl', { body: JSON.stringify({url: change.url}), method: 'POST', headers: { 'Content-Type': 'application/json' } })          
+    }
+});
